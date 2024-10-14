9b47476e346cd0a0d2a2d45296940dfe043a06fa
eparellada <parellada80@gmail.com>
2024-10-14 17:40:26 +0200

    added consonants to the code

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 # Solicitar una frase al usuario
 frase = str(input("Introduce una frase: ")).lower()
 
-# Inicializar un diccionario para contar las vocales
+# Inicializar un diccionario para contar las vocales y otro para contar las consonantes
 conteo_vocales = {}
+conteo_consonantes = {}
 
 # Definir las vocales
 vocales = "aeiou"
 
-# Contar las vocales en la frase
+# Contar las vocales y consonantes en la frase
 for letra in frase:
-    if letra in vocales:
-        if letra in conteo_vocales:
-            conteo_vocales[letra] += 1
+    if letra != " ":
+        if letra in vocales:
+            if letra in conteo_vocales:
+                conteo_vocales[letra] += 1
+            else:
+                conteo_vocales[letra] = 1
         else:
-            conteo_vocales[letra] = 1
+            if letra in conteo_consonantes:
+                conteo_consonantes[letra] +=1
+            else:
+                conteo_consonantes[letra] = 1
 
 # Mostrar el resultado
 print("Conteo de vocales:", conteo_vocales)
+print("Conteo de consonantes:", conteo_consonantes)
